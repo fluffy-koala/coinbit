7e8d5e87e3bd3744954ddbfaaa5e773521421c28
Vladyslav Verzun <vlad.verzun@itjet.io>
2023-02-26 19:27:34 +0200

    feat(authentication): add <SignUp /> screen

---
+++ src/features/authentication/screens/SignUp/SignUp.tsx
@@ -0,0 +1,48 @@
+import { Screen } from '@components';
+import { yupResolver } from '@hookform/resolvers/yup';
+import type { FC } from 'react';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import type { SignUpFormData } from '../../components';
+import { Greeting, SignUpForm } from '../../components';
+import type { AuthenticationStackScreenProps } from '../../navigation';
+import { schema } from '../../utils';
+
+const SignUp: FC<AuthenticationStackScreenProps<'SignUp'>> = ({
+  navigation,
+}) => {
+  const formMethods = useForm<SignUpFormData>({
+    defaultValues: {
+      confirmedPassword: undefined,
+      email: undefined,
+      password: undefined,
+    },
+    mode: 'onChange',
+    resolver: yupResolver(schema.signUpForm),
+  });
+
+  const handleSignInButtonPress = () => {
+    navigation.replace('SignIn');
+  };
+
+  const handleSignUpButtonPress = formMethods.handleSubmit(() => {});
+
+  return (
+    <Screen>
+      <Greeting
+        box={{ marginBottom: '52px' }}
+        subtitle="Please create a new account"
+        title="Welcome"
+      />
+      <FormProvider {...formMethods}>
+        <SignUpForm
+          onSignInButtonPress={handleSignInButtonPress}
+          onSignUpButtonPress={handleSignUpButtonPress}
+        />
+      </FormProvider>
+    </Screen>
+  );
+};
+
+export default SignUp;
