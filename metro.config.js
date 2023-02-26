e9190d6f4ca67d92fe38cdd69d573d120d0ed4a1
Vladyslav Verzun <vlad.verzun@itjet.io>
2023-02-26 19:11:44 +0200

    feat: setup .svg assets support

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,27 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => {
-      return {
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      };
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { assetExts, sourceExts },
+  } = await getDefaultConfig();
+
+  return {
+    resolver: {
+      assetExts: assetExts.filter((ext) => {
+        return ext !== 'svg';
+      }),
+      sourceExts: [...sourceExts, 'svg'],
     },
-  },
-};
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => {
+        return {
+          transform: {
+            experimentalImportSupport: false,
+            inlineRequires: true,
+          },
+        };
+      },
+    },
+  };
+})();
