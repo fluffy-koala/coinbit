4f047bf1353973acbfb7c5327a22a0274c86ca9d
Vladyslav Verzun <vlad.verzun@itjet.io>
2023-02-26 19:27:10 +0200

    feat(authentication): add <SignIn /> screen

--- src/features/authentication/screens/SignIn/SignIn.tsx
+++ src/features/authentication/screens/SignIn/SignIn.tsx
@@ -1,10 +1,47 @@
-import type { AuthenticationStackScreenProps } from '@navigation/types';
+import { Screen } from '@components';
+import { yupResolver } from '@hookform/resolvers/yup';
 import type { FC } from 'react';
 import React from 'react';
-import { View } from 'react-native';
+import { FormProvider, useForm } from 'react-hook-form';
 
-const SignIn: FC<AuthenticationStackScreenProps<'SignIn'>> = () => {
-  return <View />;
+import type { SignInFormData } from '../../components';
+import { Greeting, SignInForm } from '../../components';
+import type { AuthenticationStackScreenProps } from '../../navigation';
+import { schema } from '../../utils';
+
+const SignIn: FC<AuthenticationStackScreenProps<'SignIn'>> = ({
+  navigation,
+}) => {
+  const formMethods = useForm<SignInFormData>({
+    defaultValues: {
+      email: undefined,
+      password: undefined,
+    },
+    mode: 'onChange',
+    resolver: yupResolver(schema.signInForm),
+  });
+
+  const handleSignInButtonPress = formMethods.handleSubmit(() => {});
+
+  const handleSignUpButtonPress = () => {
+    navigation.replace('SignUp');
+  };
+
+  return (
+    <Screen>
+      <Greeting
+        box={{ marginBottom: '52px' }}
+        subtitle="Please sign in to your account"
+        title="Welcome"
+      />
+      <FormProvider {...formMethods}>
+        <SignInForm
+          onSignInButtonPress={handleSignInButtonPress}
+          onSignUpButtonPress={handleSignUpButtonPress}
+        />
+      </FormProvider>
+    </Screen>
+  );
 };
 
 export default SignIn;
--- src/features/authentication/screens/index.ts
+++ src/features/authentication/screens/index.ts
@@ -1,1 +1,2 @@
-export { SignIn } from './SignIn';
+export * from './SignIn';
+export * from './SignUp';
